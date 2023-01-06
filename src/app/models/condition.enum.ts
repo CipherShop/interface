65cc0f3130f49726dc8f218550838a211a0b174f
Fernando <fernandomartidemateo@hotmail.com>
2023-01-06 22:44:21 +0100

    updated models

---
+++ src/app/models/condition.enum.ts
@@ -0,0 +1,5 @@
+export enum ECondition {
+    New = 1,
+    Used = 2,
+    Other = 3,
+}
--- src/app/models/item.object.ts
+++ src/app/models/item.object.ts
@@ -1,18 +1,24 @@
+import { ECondition } from "./condition.enum";
+
 export interface IItem {
-    uid: number;
+    UID: number;
     title: string;
     description: string;
-    seller: any;
-    prices: number[];
+    seller: string; //name of ISeller
+    price: number;
+    isErc20Price: boolean; //true -> erc-20, false -> ETH
+    erc20: string;
+    admitBids: boolean;
     bid: number;
     bids: any[];
-    erc20: string;
     images: string[];
     quantity: number;
-    watch: string[];
     sold: number;
-    condition: number;
-    shipping: number[];
-    delivery: number;
-    returns: number;
+    watch: string[]; //name of IUser
+    condition: ECondition;
+    localization: number;
+    shippingTo: number[];
+    deliveryPrice: number;
+    admitsReturns: boolean;
+    returns: Date;
 }
